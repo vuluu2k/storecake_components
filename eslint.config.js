8e9b1c0376dd790e01236e984c3517a96e234d75
vuluu <vuluu040320@gmail.com>
2025-07-26 15:07:16 +0700

    update

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,13 @@
 // eslint.config.js
-export default {
-  extends: ['plugin:@typescript-eslint/recommended', 'plugin:vue/vue3-recommended', 'prettier'],
-}
+import { defineConfig } from 'eslint/config'
+
+export default defineConfig([
+  {
+    files: ['src/**/*.{js,jsx,vue}'],
+    extends: ['eslint:recommended', 'plugin:vue/vue3-recommended', 'prettier'],
+  },
+  {
+    files: ['src/**/*.{ts,tsx}'],
+    extends: ['plugin:@typescript-eslint/recommended', 'prettier'],
+  },
+])
