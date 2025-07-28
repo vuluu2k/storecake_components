268db5ce6b2aea535543d70293a34dca30830013
vuluu2k <vuluu040320@gmail.com>
2025-07-28 07:39:01 +0000

    Deploying to docs from @ vuluu2k/storecake_components@59b5d07b0a98750a6004e91dd87526f70686db12 🚀

---
+++ assets/Button.stories-CvXW19Nn.js
@@ -0,0 +1,72 @@
+import{B as u}from"./Button-D-CVmOkw.js";import"./iframe-DDK_JI2u.js";import"./index-VtadNko8.js";import"./_plugin-vue_export-helper-DnytYQB0.js";import"./_commonjsHelpers-Cpj98o6Y.js";const S={title:"Auto/Button",component:u,tags:["autodocs"],argTypes:{label:{control:"text",description:"Label of the Button"},loading:{control:"boolean",description:"Loading state of the Button"},type:{control:"select",options:["primary","secondary","gray","info","warning","black","gold","error","danger"],description:"Type of the Button"},size:{control:"select",options:["sm","md","lg"],description:"Size of the Button"},danger:{control:"boolean",description:"Danger state of the Button"},ghost:{control:"boolean",description:"Ghost state of the Button"},disabled:{control:"boolean",description:"Disabled state of the Button"},classes:{control:"text",description:"Classes of the Button"}},args:{label:"",loading:!1,type:"primary",size:"md",danger:!1,ghost:!1,disabled:!1,classes:""}},e={args:{label:"",loading:!1,type:"primary",size:"md",danger:!1,ghost:!1,disabled:!1,classes:""}},r={args:{label:"Button",type:"primary"}},a={args:{label:"Button",type:"secondary"}},s={args:{label:"Button",type:"gray"}},o={args:{label:"Button",type:"info"}},t={args:{label:"Button",type:"warning"}},n={args:{label:"Button",type:"black"}},c={args:{label:"Button",type:"gold"}},l={args:{label:"Button",type:"error"}},p={args:{label:"Button",type:"danger"}},d={args:{label:"Button",size:"sm"}},i={args:{label:"Button",size:"md"}},m={args:{label:"Button",size:"lg"}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: '',
+    loading: false,
+    type: 'primary',
+    size: 'md',
+    danger: false,
+    ghost: false,
+    disabled: false,
+    classes: ''
+  }
+}`,...e.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: 'Button',
+    type: 'primary'
+  }
+}`,...r.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: 'Button',
+    type: 'secondary'
+  }
+}`,...a.parameters?.docs?.source}}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: 'Button',
+    type: 'gray'
+  }
+}`,...s.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: 'Button',
+    type: 'info'
+  }
+}`,...o.parameters?.docs?.source}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: 'Button',
+    type: 'warning'
+  }
+}`,...t.parameters?.docs?.source}}};n.parameters={...n.parameters,docs:{...n.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: 'Button',
+    type: 'black'
+  }
+}`,...n.parameters?.docs?.source}}};c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: 'Button',
+    type: 'gold'
+  }
+}`,...c.parameters?.docs?.source}}};l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: 'Button',
+    type: 'error'
+  }
+}`,...l.parameters?.docs?.source}}};p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: 'Button',
+    type: 'danger'
+  }
+}`,...p.parameters?.docs?.source}}};d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: 'Button',
+    size: 'sm'
+  }
+}`,...d.parameters?.docs?.source}}};i.parameters={...i.parameters,docs:{...i.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: 'Button',
+    size: 'md'
+  }
+}`,...i.parameters?.docs?.source}}};m.parameters={...m.parameters,docs:{...m.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: 'Button',
+    size: 'lg'
+  }
+}`,...m.parameters?.docs?.source}}};const h=["Default","Primary","Secondary","Gray","Info","Warning","Black","Gold","Error","Danger","Sm","Md","Lg"];export{n as Black,p as Danger,e as Default,l as Error,c as Gold,s as Gray,o as Info,m as Lg,i as Md,r as Primary,a as Secondary,d as Sm,t as Warning,h as __namedExportsOrder,S as default};
