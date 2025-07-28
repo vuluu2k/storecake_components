6726e53d6b3f82c3ae24335218384eaba4834a82
vuluu2k <vuluu040320@gmail.com>
2025-07-28 07:25:52 +0000

    Deploying to docs from @ vuluu2k/storecake_components@025a5d465700ff289276801a2d16ced90560357a 🚀

---
+++ assets/Button.stories-BqEG_Yji.js
@@ -0,0 +1,61 @@
+import{B as i}from"./Button-i-vO8Cwt.js";import"./iframe-e4iVdjZY.js";import"./_plugin-vue_export-helper-xdCF7JtS.js";import"./_commonjsHelpers-Cpj98o6Y.js";const N={title:"Auto/Button",component:i,tags:["autodocs"],argTypes:{label:{control:"text",description:"Label of the Button"},loading:{control:"boolean",description:"Loading state of the Button"},type:{control:"select",options:["primary","secondary","gray","info","warning","black","gold","error","danger"],description:"Type of the Button"},size:{control:"select",options:["sm","md","lg"],description:"Size of the Button"},danger:{control:"boolean",description:"Danger state of the Button"},ghost:{control:"boolean",description:"Ghost state of the Button"},disabled:{control:"boolean",description:"Disabled state of the Button"},classes:{control:"text",description:"Classes of the Button"}},args:{label:"",loading:!1,type:"primary",size:"md",danger:!1,ghost:!1,disabled:!1,classes:""}},e={args:{label:`${componentName}`,type:"primary"}},a={args:{label:`${componentName}`,type:"secondary"}},r={args:{label:`${componentName}`,type:"gray"}},o={args:{label:`${componentName}`,type:"info"}},s={args:{label:`${componentName}`,type:"warning"}},n={args:{label:`${componentName}`,type:"black"}},t={args:{label:`${componentName}`,type:"gold"}},c={args:{label:`${componentName}`,type:"error"}},m={args:{label:`${componentName}`,type:"danger"}},p={args:{label:`${componentName}`,size:"sm"}},l={args:{label:`${componentName}`,size:"md"}},d={args:{label:`${componentName}`,size:"lg"}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: \`\${componentName}\`,
+    type: 'primary'
+  }
+}`,...e.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: \`\${componentName}\`,
+    type: 'secondary'
+  }
+}`,...a.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: \`\${componentName}\`,
+    type: 'gray'
+  }
+}`,...r.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: \`\${componentName}\`,
+    type: 'info'
+  }
+}`,...o.parameters?.docs?.source}}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: \`\${componentName}\`,
+    type: 'warning'
+  }
+}`,...s.parameters?.docs?.source}}};n.parameters={...n.parameters,docs:{...n.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: \`\${componentName}\`,
+    type: 'black'
+  }
+}`,...n.parameters?.docs?.source}}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: \`\${componentName}\`,
+    type: 'gold'
+  }
+}`,...t.parameters?.docs?.source}}};c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: \`\${componentName}\`,
+    type: 'error'
+  }
+}`,...c.parameters?.docs?.source}}};m.parameters={...m.parameters,docs:{...m.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: \`\${componentName}\`,
+    type: 'danger'
+  }
+}`,...m.parameters?.docs?.source}}};p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: \`\${componentName}\`,
+    size: 'sm'
+  }
+}`,...p.parameters?.docs?.source}}};l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: \`\${componentName}\`,
+    size: 'md'
+  }
+}`,...l.parameters?.docs?.source}}};d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: \`\${componentName}\`,
+    size: 'lg'
+  }
+}`,...d.parameters?.docs?.source}}};const $=["Primary","Secondary","Gray","Info","Warning","Black","Gold","Error","Danger","Sm","Md","Lg"];export{n as Black,m as Danger,c as Error,t as Gold,r as Gray,o as Info,d as Lg,l as Md,e as Primary,a as Secondary,p as Sm,s as Warning,$ as __namedExportsOrder,N as default};
