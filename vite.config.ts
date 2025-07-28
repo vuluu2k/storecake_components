ca7d22a806675f78c929a2ca0ec37fdf929c49c6
vuluu <vuluu040320@gmail.com>
2025-07-28 12:20:18 +0700

    clear package

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import vue from '@vitejs/plugin-vue';
 import path, { resolve } from 'path';
 import { fileURLToPath } from 'node:url';
 import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
-import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -13,10 +12,6 @@ const __dirname = path.dirname(__filename);
 export default defineConfig({
   plugins: [
     vue(),
-    // Tự động inject CSS vào file JS khi build
-    cssInjectedByJsPlugin({
-      relativeCSSInjection: true,
-    }),
   ],
   resolve: {
     alias: {
@@ -25,29 +20,6 @@ export default defineConfig({
       '@stories': resolve(__dirname, './src/stories'),
       '@assets': resolve(__dirname, './src/assets'),
     },
-  },
-  // Cấu hình build cho thư viện
-  build: {
-    lib: {
-      // Điểm vào cho thư viện
-      entry: resolve(__dirname, 'src/main.ts'),
-      name: 'StoreCakeComponents',
-      fileName: (format) => `storecake-components.${format}.js`,
-    },
-    rollupOptions: {
-      // Đảm bảo các thư viện bên ngoài được xử lý đúng cách
-      external: ['vue'],
-      output: {
-        // Cung cấp các biến toàn cục để sử dụng trong UMD build
-        globals: {
-          vue: 'Vue',
-        },
-      },
-    },
-    // Tạo sourcemap để dễ debug
-    sourcemap: true,
-    // Giảm kích thước file output
-    minify: 'terser',
   },
   test: {
     projects: [{
