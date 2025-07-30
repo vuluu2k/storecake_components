edd8d4f9300fd0fbaed6be596955ead5bca6d8b4
vuluu <vuluu040320@gmail.com>
2025-07-30 17:55:14 +0700

    update

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -15,6 +15,12 @@ const config: StorybookConfig = {
   "framework": {
     "name": "@storybook/vue3-vite",
     "options": {}
-  }
+  },
+  managerHead: (head) => `
+    ${head}
+    <link rel="icon" href="https://content.pancake.vn/1.1/s450x450/fwebp/87/12/e9/86/59eb6fdc125b4840df72b830615bafd86e3bfcc3bbf6a92beef2efca.png">
+    <link rel="mask-icon" href="https://content.pancake.vn/1.1/s450x450/fwebp/87/12/e9/86/59eb6fdc125b4840df72b830615bafd86e3bfcc3bbf6a92beef2efca.png" type="image/png">
+    <link rel="apple-touch-icon" href="https://content.pancake.vn/1.1/s450x450/fwebp/87/12/e9/86/59eb6fdc125b4840df72b830615bafd86e3bfcc3bbf6a92beef2efca.png" type="image/png">
+  `,
 };
 export default config;
