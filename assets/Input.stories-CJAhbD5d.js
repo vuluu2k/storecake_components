35a0a390e2696f7c861b84356e94e760fd9a2d06
vuluu2k <vuluu040320@gmail.com>
2025-07-30 10:52:19 +0000

    Deploying to docs from @ vuluu2k/storecake_components@2bf228a0413ba8ac89fb198a0710ae3532544384 🚀

---
+++ assets/Input.stories-CJAhbD5d.js
@@ -0,0 +1,35 @@
+import{I as t}from"./Input-Bv63a6ar.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./iframe-DK_PKbSa.js";import"./PhQuestion.vue-BqBO78VS.js";import"./default-BL9zUjJC.js";import"./index-D1Bhqelu.js";import"./wave-Db07UHnn.js";import"./initDefaultProps-OKg-6tR5.js";import"./classCallCheck-8_osy8gf.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./vnode-CCy8Nxxe.js";import"./index-nV21dyyK.js";import"./Trigger-4lVq9b8I.js";import"./SearchOutlined-B_c5GxIm.js";import"./FormItemContext-CnXKYLxr.js";import"./index-CQ_-iQpM.js";import"./colors-B30ZPdXG.js";const z={title:"Auto/Input",component:t,tags:["autodocs"],argTypes:{value:{control:"number",description:"Property value of the Input"},classes:{control:"text",description:"Classes of the Input"},placeholder:{control:"text",description:"Property placeholder of the Input"},size:{control:"select",options:["sm","md","lg"],description:"Size of the Input"},label:{control:"text",description:"Label of the Input"},labelInfo:{control:"text",description:"Property labelInfo of the Input"},questionInfo:{control:"text",description:"Property questionInfo of the Input"},isError:{control:"boolean",description:"Property isError of the Input"},isLink:{control:"boolean",description:"Property isLink of the Input"},message:{control:"text",description:"Property message of the Input"},id:{control:"text",description:"Property id of the Input"},disabled:{control:"boolean",description:"Disabled state of the Input"},allowClear:{control:"boolean",description:"Property allowClear of the Input"},isTextArea:{control:"boolean",description:"Property isTextArea of the Input"},rows:{control:"number",description:"Property rows of the Input"},useDebounce:{control:"boolean",description:"Property useDebounce of the Input"},require:{control:"boolean",description:"Property require of the Input"},isNumber:{control:"boolean",description:"Property isNumber of the Input"}},args:{classes:"",placeholder:"",size:"md",label:"",labelInfo:"",questionInfo:"",isError:!1,isLink:!1,message:"",id:"",disabled:!1,allowClear:!1,rows:2,useDebounce:!1,require:!1,isNumber:!1}},e={args:{classes:"",placeholder:"",size:"md",label:"Username",labelInfo:"",questionInfo:"",isError:!1,isLink:!1,message:"",id:"",disabled:!1,allowClear:!1,rows:2,useDebounce:!1,require:!1,isNumber:!1}},o={args:{size:"sm",label:"Username"}},r={args:{size:"md",label:"Username"}},s={args:{size:"lg",label:"Username"}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    classes: '',
+    placeholder: '',
+    size: 'md',
+    label: 'Username',
+    labelInfo: '',
+    questionInfo: '',
+    isError: false,
+    isLink: false,
+    message: '',
+    id: '',
+    disabled: false,
+    allowClear: false,
+    rows: 2,
+    useDebounce: false,
+    require: false,
+    isNumber: false
+  }
+}`,...e.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    size: 'sm',
+    label: 'Username'
+  }
+}`,...o.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    size: 'md',
+    label: 'Username'
+  }
+}`,...r.parameters?.docs?.source}}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    size: 'lg',
+    label: 'Username'
+  }
+}`,...s.parameters?.docs?.source}}};const q=["Default","Sm","Md","Lg"];export{e as Default,s as Lg,r as Md,o as Sm,q as __namedExportsOrder,z as default};
--- assets/Switch.stories-C_erMing.js
+++
@@ -1,9 +0,0 @@
-import{S as t}from"./Switch-DlKxuwfa.js";import"./default-B8PE3y3o.js";import"./iframe-DJ4dcJs0.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./KeyCode-CjcKJgc-.js";import"./wave-DC1v0264.js";import"./FormItemContext-BLYdGYol.js";const n={title:"Auto/Switch",component:t,tags:["autodocs"],argTypes:{checked:{control:"boolean",description:"Property checked of the Switch"},label:{control:"text",description:"Label of the Switch"},subLabel:{control:"text",description:"Property subLabel of the Switch"},size:{control:"text",description:"Size of the Switch"},disabled:{control:"boolean",description:"Disabled state of the Switch"}},args:{checked:!1,label:"",subLabel:"",size:"md",disabled:!1}},e={args:{checked:!1,label:"",subLabel:"",size:"md",disabled:!1}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {
-    checked: false,
-    label: '',
-    subLabel: '',
-    size: 'md',
-    disabled: false
-  }
-}`,...e.parameters?.docs?.source}}};const p=["Default"];export{e as Default,p as __namedExportsOrder,n as default};
