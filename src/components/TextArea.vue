e8ae6f6f646ae6de892fcebb07453bb138180556
vuluu <vuluu040320@gmail.com>
2025-08-01 11:36:04 +0700

    update

---
+++ src/components/TextArea.vue
@@ -0,0 +1,153 @@
+<template>
+  <div class="input-design" :class="$attrs.class">
+    <label
+      v-if="label"
+      :class="['input-label-design', { 'input-label-design-error': isError, 'input-label-design-link': isLink }]"
+    >
+      <span>{{ label }}</span>
+      <a-tooltip :title="labelInfo">
+        <PhInfo v-if="labelInfo" weight="fill" />
+      </a-tooltip>
+    </label>
+    <a-textarea
+      v-bind="{ ...$attrs, class: undefined }"
+      ref="input"
+      :class="[
+        'input-design-input',
+        size,
+        { 'input-design-input-error': isError, 'input-design-input-link': isLink, disabled },
+        classes,
+      ]"
+      :value="value"
+      :placeholder="placeholder"
+      :allow-clear="false"
+      @change="handleChange"
+      @focus="handleFocus"
+      @blur="handleBlur"
+      @keypress="handleKeyPress"
+    ></a-textarea>
+    <div v-if="message" :class="['input-message-design', { 'input-message-design-error': isError }]">
+      <span class="input-message-icon">
+        <PhInfo weight="fill" />
+      </span>
+      <span class="input-message-text">{{ message }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+  import { debounce } from 'lodash'
+  import { PhInfo } from '@phosphor-icons/vue'
+  export default {
+    components: {
+      PhInfo,
+    },
+    inheritAttrs: false,
+    props: {
+      value: {
+        type: [String, Number],
+        default: undefined,
+      },
+      classes: {
+        type: String,
+        default: '',
+      },
+      placeholder: {
+        type: String,
+        default: '',
+      },
+      size: {
+        type: String,
+        default: 'md',
+        validator: (value) => {
+          return ['sm', 'md', 'lg'].includes(value)
+        },
+      },
+      label: {
+        type: String,
+        default: '',
+      },
+      labelInfo: {
+        type: String,
+        default: '',
+      },
+      questionInfo: {
+        type: String,
+        default: '',
+      },
+      isError: {
+        type: Boolean,
+        default: false,
+      },
+      isLink: {
+        type: Boolean,
+        default: false,
+      },
+      message: {
+        type: [String, Boolean],
+        default: '',
+      },
+      id: {
+        type: String,
+        default: '',
+      },
+      disabled: {
+        type: Boolean,
+        default: false,
+      },
+      allowClear: {
+        type: Boolean,
+        default: false,
+      },
+      useDebounce: {
+        type: Boolean,
+        default: false,
+      },
+    },
+    emits: ['change', 'update:value', 'focus', 'blur', 'pressEnter', 'keypress'],
+    setup() {
+      return {}
+    },
+    methods: {
+      handleChange(e) {
+        const value = e.target.value
+        this.$emit('update:value', value)
+
+        if (this.useDebounce) {
+          this.handleDebounceEmit(value, e)
+        } else {
+          this.$emit('change', value, e)
+        }
+      },
+      handleFocus(e) {
+        const value = e.target.value
+        this.$emit('focus', value, e)
+      },
+      handleBlur(e) {
+        const value = e.target.value
+        this.$emit('blur', value, e)
+      },
+      focus() {
+        this.$refs.input.focus()
+      },
+      blur() {
+        this.$refs.input.blur()
+      },
+      handleClear() {
+        this.$emit('update:value', '')
+        this.$emit('change', '')
+      },
+      handleDebounceEmit: debounce(function (value, $event) {
+        this.$emit('change', value, $event)
+      }, 500),
+      handleKeyPress(e) {
+        this.$emit('keypress', e)
+        const keyCode = e.keyCode || e.which
+        const value = e.target.value
+        if (keyCode === 13) {
+          this.$emit('pressEnter', value, e)
+        }
+      },
+    },
+  }
+</script>
---
+++ src/stories/TextArea.stories.ts
@@ -0,0 +1,78 @@
+import type { Meta, StoryObj } from '@storybook/vue3-vite'
+
+import TextArea from '../components/TextArea.vue'
+
+const meta = {
+  title: 'Auto/TextArea',
+  component: TextArea,
+  tags: ['autodocs'],
+  argTypes: {
+    value: { control: 'number', description: 'Property value of the TextArea' },
+    classes: { control: 'text', description: 'Classes of the TextArea' },
+    placeholder: { control: 'text', description: 'Property placeholder of the TextArea' },
+    size: { control: 'select', options: ['sm', 'md', 'lg'], description: 'Size of the TextArea' },
+    label: { control: 'text', description: 'Label of the TextArea' },
+    labelInfo: { control: 'text', description: 'Property labelInfo of the TextArea' },
+    questionInfo: { control: 'text', description: 'Property questionInfo of the TextArea' },
+    isError: { control: 'boolean', description: 'Property isError of the TextArea' },
+    isLink: { control: 'boolean', description: 'Property isLink of the TextArea' },
+    message: { control: 'text', description: 'Property message of the TextArea' },
+    id: { control: 'text', description: 'Property id of the TextArea' },
+    disabled: { control: 'boolean', description: 'Disabled state of the TextArea' },
+    allowClear: { control: 'boolean', description: 'Property allowClear of the TextArea' },
+    useDebounce: { control: 'boolean', description: 'Property useDebounce of the TextArea' },
+  },
+  args: {
+    classes: '',
+    placeholder: '',
+    size: 'md',
+    label: '',
+    labelInfo: '',
+    questionInfo: '',
+    isError: false,
+    isLink: false,
+    message: '',
+    id: '',
+    disabled: false,
+    allowClear: false,
+    useDebounce: false,
+  },
+} satisfies Meta<typeof TextArea>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    classes: '',
+    placeholder: '',
+    size: 'md',
+    label: '',
+    labelInfo: '',
+    questionInfo: '',
+    isError: false,
+    isLink: false,
+    message: '',
+    id: '',
+    disabled: false,
+    allowClear: false,
+    useDebounce: false,
+  },
+}
+
+export const Sm: Story = {
+  args: {
+    size: 'sm',
+  },
+}
+export const Md: Story = {
+  args: {
+    size: 'md',
+  },
+}
+export const Lg: Story = {
+  args: {
+    size: 'lg',
+  },
+}
